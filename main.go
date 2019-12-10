127a3f5f62e6aef21e86261d0deca640481b412d
Vincent Villaluna <villalunavincent@gmail.com>
2019-12-10 10:24:52 +0800

    added colly for crawling git.ir

---
+++ main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/gocolly/colly"
+)
+
+var counter = 0
+
+func main() {
+	c := colly.NewCollector(
+		colly.AllowedDomains("git.ir"),
+	)
+
+	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
+		title := e.Attr("title")
+		if title == "دانلود Download" {
+			file, err := os.OpenFile("url.txt", os.O_APPEND|os.O_WRONLY, 0664)
+			if err != nil {
+				fmt.Println(err)
+			}
+			defer file.Close()
+			if _, err = file.WriteString(e.Attr("href") + "\n"); err != nil {
+				fmt.Println(err)
+			}
+			counter++
+			fmt.Printf("Getting url of video %d \n", counter)
+		}
+	})
+
+	c.OnRequest(func(r *colly.Request) {
+		fmt.Println("Visiting", r.URL.String())
+	})
+
+	c.Visit("https://git.ir/skillshare-build-your-first-app-with-react-native-and-expo/")
+}
